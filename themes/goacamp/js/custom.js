a95fb3fdbd7a39e9faccb8eda70670f430e110d9
Manjitsingh <manjit7193@gmail.com>
2018-01-14 22:00:41 +0530

    Responsive site css

--- themes/goacamp/js/custom.js
+++ themes/goacamp/js/custom.js
@@ -1,6 +1,20 @@
 (function ($) {
     Drupal.behaviors.exampleModule = {
         attach: function (context, settings) {
+            if ($(window).width() < 767) {
+                var main_menu = '.menu--main .menu';
+                var login_menu = '.menu--account ul.menu';
+                var login_menu_html = $(login_menu).html();
+                $('.menu--main > ul.menu').append(login_menu_html);
+                $('.menu--main .menu').hide();
+                $('.menu--main').click(function() {
+                    if($(main_menu).css('display') == 'none') {
+                        $(main_menu).show();
+                    } else {
+                        $(main_menu).hide();
+                    }
+                });
+            }
         }
     };
 }(jQuery));
